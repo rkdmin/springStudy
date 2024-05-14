f0de9e5c91a736090e890aa5220502cc5be45438
LeeKangMin <22rkdmin@thinkfree.com>
2024-05-14 15:41:59 +0900

    서블릿 생성

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 
 public class DemoApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		// 여러 개의 servlet이 있는 servletContext 생성
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("servlet", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					// 응답 코드
+					resp.setStatus(HttpStatus.OK.value());
+					// 헤더 설정
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().print("Hello " + name);
+				}
+			}).addMapping("/*");
+		});
 		webServer.start();
 	}
 }
