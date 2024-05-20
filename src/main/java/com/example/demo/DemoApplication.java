07bd2449076e8596d8f5ca66b034a2ae1fc803fa
LeeKangMin <22rkdmin@thinkfree.com>
2024-05-20 11:27:13 +0900

    메인 리팩토링

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,30 +3,52 @@ package com.example.demo;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@ComponentScan
+@Configuration// 빈 등록하기 위해 사용 (*)
+@ComponentScan// 컴포넌트 스캔 (**)
 public class DemoApplication {
+	// 톰켓 빈 생성 (***)
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory(){
+		return new TomcatServletWebServerFactory();
+	}
+
+	// 디스패쳐 서블릿 빈 생성 (***)
+ 	@Bean
+	public DispatcherServlet dispatcherServlet(){
+		return new DispatcherServlet();
+	}
+
 	public static void main(String[] args) {
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext ();
-		context.register(DemoApplication.class);
+		// (****) 로직을 run으로 빼줌
+		run(DemoApplication.class);
+	}
 
-		// 직접 빈으로 등록하지 않음
-//		context.registerBean(PostController.class);
-//		context.registerBean(PostService.class);
-//		context.registerBean(PostRepository.class);
+	private static void run(Class<?> applicationClass) {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				// 톰켓, 디스패쳐 서블릿 빈 사용 (*****)
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+							.addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
+
+		context.register(applicationClass);
 		context.refresh();
-
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			// 스프링 컨테스트를 인자로 받는 DispatcherServlet으로 변경(*)
-			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(context)
-					).addMapping("/*");
-		});
-		webServer.start();
 	}
 }
